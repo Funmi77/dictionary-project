613dc682fbe02402064df758f2e3b1d11b91ff5d
Funmi77 <vrcreativeagency@gmail.com>
2026-05-17 02:02:22 +0100

    Created Search Engine Form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import "./App.css";
 import logo from "./logo.png";
+import Dictionary from "./Dictionary";
+
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+
+        <main>
+          <Dictionary />
+        </main>
+
+        <footer className="App-footer">
+          <small>
+            Coded by{" "}
+            <a
+              href="https://github.com/Funmi77"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Funmilayo Fagbemi
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://github.com/Funmi77/react-weather-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              open-sourced on GitHub
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
