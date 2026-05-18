8fb8b7acb3c40de5fa2b7e6f783e327adca71961
Funmi77 <vrcreativeagency@gmail.com>
2026-05-18 15:54:19 +0100

    Added API Documentation link

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -12,6 +12,8 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
 
+    //documentation:https://www.shecodes.io/learn/apis/dictionary
+
     let apiKey = "7e77fbbbab91e5504tfaaa75643of118";
 
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
