11f50a58845cecec5021bd3dadbbb96fe2ffcd16
Funmi77 <vrcreativeagency@gmail.com>
2026-05-18 19:27:15 +0100

    Rendered the word meanings

---
+++ src/Results.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Meaning from "./Meaning";
+
+export default function Results(props) {
+  if (!props.results) {
+    return null;
+  }
+
+  return (
+    <div className="Results">
+      <h2>{props.results.word}</h2>
+
+      {props.results.meanings?.map((meaning, index) => (
+        <div key={index}>
+          <Meaning meaning={meaning} />
+        </div>
+      ))}
+    </div>
+  );
+}
